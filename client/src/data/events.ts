09a27e3ba74c2e6b3ca475a2b1dacb4219db98b7
malini160806 <maliniravanan@gmail.com>
2026-08-27 00:26:38 +0530

    works

--- client/src/data/events.ts
+++ client/src/data/events.ts
@@ -312,7 +312,7 @@ export const EVENTS: EventItem[] = [
   },
   {
     id: 'poster-designing',
-    title: ' visual vanguard',
+    title: 'visual ',
     category: 'non-technical',
     description:
       'Participants design an attractive and impactful poster based on a given theme or topic within a specified time, using creativity, artistic expression, visual communication and innovative thinking.',
